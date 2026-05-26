47c87b63160869c3284f92c079236b556d9367a9
qhangz <qhangz@163.com>
2026-05-26 15:10:25 +0800

    Add project information

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * 项目名称：s045Interface
+ * 开发者：李仕
+ * 所属组织：xLab-HDU
+ * 代码仓库：https://github.com/xLab-HDU
+ * 
+ * 本代码为开源项目，欢迎提交 Issue 和 Pull Request。
+ */
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
